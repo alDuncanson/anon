3df9bd9badbcf54dbad3e802326d00c7e8b63562
Al Duncanson <alexjduncanson@gmail.com>
2018-11-21 13:29:58 -0500

    added google user authentication

--- user-feed/src/App.js
+++ user-feed/src/App.js
@@ -1,13 +1,69 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import { auth, provider } from './firebase'
 import './App.css'
-import Login from './components/Login'
 
-export default class App extends Component {
+const styles = theme => ({
+    container: {
+        margin: '0 0 0 auto',
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    button: {
+        margin: theme.spacing.unit,
+    }
+})
+
+class App extends Component {
+
+    state = {
+        user: null
+    }
+
+    login = () => {
+        auth.signInWithPopup(provider)
+            .then((result) => {
+                this.setState({
+                    user: result.user
+                })
+            })
+    }
+
+    logout = () => {
+        auth.signOut()
+            .then(() => {
+                this.setState({
+                    user: null
+                })
+            })
+    }
+
     render() {
+        const { classes } = this.props
         return (
             <div className='app'>
-                <Login/>
+                <AppBar position="static" color="default">
+                    <Toolbar>
+                        <Typography variant="h6" color="inherit">
+                            App Title
+                        </Typography>
+                        <form className={classes.container} noValidate autoComplete='off'>
+                            {this.state.user ? <Button variant="outlined" className={classes.button} onClick={this.logout}>Log Out</Button> : <Button variant="outlined" className={classes.button} onClick={this.login}>Log In</Button>}
+                        </form>
+                    </Toolbar>
+                </AppBar>
             </div>
         )
     }
 }
+
+App.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(App)
--- user-feed/src/components/Login.js
+++ user-feed/src/components/Login.js
@@ -1,79 +1,15 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
 import './Login.css'
 
-const styles = theme => ({
-    container: {
-        margin: '54px auto 0 auto',
-        width: '300px',
-        display: 'flex',
-        flexDirection: 'column'
-    },
-    textField: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit
-    },
-    button: {
-        margin: theme.spacing.unit,
-    }
-})
 
-class Login extends Component {
 
-    state = {
-        name: null,
-        password: null
-    }
-
-    handleChange = name => event => {
-        this.setState({
-            [name]: event.target.value,
-        })
-    }
+export default class Login extends Component {
 
     render() {
-        const { classes } = this.props
         return (
-            <Fragment>
-                <AppBar position="static" color="default">
-                    <Toolbar>
-                        <Typography variant="h6" color="inherit">
-                            App Title
-                    </Typography>
-                    </Toolbar>
-                </AppBar>
-                <form className={classes.container} noValidate autoComplete='off'>
-                    <TextField
-                        id='username'
-                        label='Name'
-                        className={classes.textField}
-                        value={this.state.name}
-                        onChange={this.handleChange('name')}
-                    />
-                    <TextField
-                        id='password'
-                        label='Password'
-                        className={classes.textField}
-                        value={this.state.password}
-                        onChange={this.handleChange('password')}
-                    />
-                    <Button variant="outlined" className={classes.button}>
-                        Submit
-                </Button>
-                </form>
-            </Fragment>
+            <Fragment/>
         )
     }
 }
-
-Login.propTypes = {
-    classes: PropTypes.object.isRequired,
-}
-
-export default withStyles(styles)(Login)
