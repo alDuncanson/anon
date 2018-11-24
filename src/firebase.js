e91a0d6699e1ade1983f073ca0be9b3a382cb89c
Al Duncanson <alexjduncanson@gmail.com>
2018-11-24 15:37:39 -0500

    removed firebase config variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import firebase from 'firebase'
+import firebaseConfig from './firebaseConfig'
 
-const config = {
-    apiKey: 'AIzaSyDcyfdg5qzoEbM-9v7Q6WwpGJQIZqfQQRU',
-    authDomain: 'user-feed-84a20.firebaseapp.com',
-    databaseURL: 'https://user-feed-84a20.firebaseio.com',
-    projectId: 'user-feed-84a20',
-    storageBucket: 'user-feed-84a20.appspot.com',
-    messagingSenderId: '969646773326'
-}
-
-firebase.initializeApp(config)
+firebase.initializeApp(firebaseConfig)
 export const provider = new firebase.auth.GoogleAuthProvider()
 export const auth = firebase.auth()
 export default firebase
---
+++ src/firebaseConfig.js
@@ -0,0 +1,5 @@
+const config = {
+    // Your firebase configuration variables
+}
+
+export default config
